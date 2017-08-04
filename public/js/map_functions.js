0a1510d2653ff7c980805e81b9329f2dd3345300
lucasmurphey <lucasmurphey@gmail.com>
2017-08-04 00:28:55 -0400

    ..

--- public/js/map_functions.js
+++ public/js/map_functions.js
@@ -71,9 +71,9 @@ function initMap() {
             map: map
           });
 
-          console.log(response[i].availability);
+          console.log(response[i].location_availability);
 
-          location.availability = response[i].availability;
+          location.availability = response[i].location_availability;
 
           google.maps.event.addListener(location.marker, 'click', function() { 
             document.getElementById("availability_window").textContent = this.availability + " Spaces Left";
