7251ca7fe191d20e6426f3e2d13838154c64c122
lucasmurphey <lucasmurphey@gmail.com>
2017-08-04 00:19:01 -0400

    ..

--- public/js/map_functions.js
+++ public/js/map_functions.js
@@ -64,8 +64,8 @@ function initMap() {
         //Make Markers
         for (var i = 0; i < response.length; i++) {
           var latLng = new google.maps.LatLng(response[i].location_lat,response[i].location_lon);
-          var location = {};
-          var marker = new google.maps.Marker({
+          var location = new Location({});
+          var location.marker = new google.maps.Marker({
             position: latLng,
             map: map
           });
